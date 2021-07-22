b7e7228c0b2cd372862e211cb21e6bb2a80214de
AnasElgarhy <44965145+Anas-Elgarhy@users.noreply.github.com>
2021-07-22 19:51:23 +0200

    Solving the problem of considering any number as a String or character regardless of whether it is a valid number or not, improving the appearance of the code and speed of execution, and adding a document (javaDoc)

--- src/MainPackage/Functions.java
+++ src/MainPackage/Functions.java
@@ -1,33 +1,55 @@
 package MainPackage;
 
 public abstract class Functions {
+    private static boolean prospectFloatNumber, minusNumber;
+    /**
+     * This function checks if the line is a number or not,
+     * and this function is supposed to work with decimal numbers and negative numbers without problems
+     * @param line as string
+     * @return true if the line is valid number false if line not a valid number
+     */
     public static boolean checkNumber(String line){
         int dot = numOfRebate('.', line, false);
         int e = numOfRebate('E', line, true);
         if(dot > 1 || e > 1)
             return false;
-        boolean minusNumber = line.startsWith("-");
-        for(int i= (minusNumber)? 1 : 0; i<line.length(); i++){
-            if(line.charAt(i) > 57 || (line.charAt(i) < 48) && dot == 1 && !(line.charAt(i) == '.') && line.length() > 1) {
-                if (dot == 1 && e == 1 && String.valueOf(line.charAt(i)).equalsIgnoreCase("E")) {
-                    if (line.charAt(i + 1) == '-')
+        minusNumber = line.startsWith("-");
+        prospectFloatNumber = (dot == 1 && e == 0) || (dot == 1 && e == 1); // check prospect float number
+        char ch;
+        for(int i= (minusNumber)? 1 : 0; i<line.length(); i++){ // loop on characters
+            ch = line.charAt(i);
+            if(ch > 57 || ch < 48) { // Verify that the letter is not within the numbers in the ASCII table
+                if(prospectFloatNumber && ch == '.' && i + 1 < line.length())
+                    i++;
+                else if (prospectFloatNumber && String.valueOf(ch).equalsIgnoreCase("E")) { // check e if prospect tempCht number
+                    if ((line.length() > i + 1) &&  line.charAt(i+1) == '-') {
                         i++;
+                        minusNumber = true;
+                    }
                 } else
                     return false;
-            } else
-                return false;
+            }
         }
         // the input is valid number
         return true;
     }
+
+    /**
+     * You are supposed to call this function after verifying that the line entered by the user is a valid number
+     * with the checkNumber function and if you do not want to use the checkNumber function
+     * then make the {@code prospectFloatNumber} and {@code minusNumber} local variables I have the checkNumber function
+     * because there is no longer a need to make them global, and do Simple loop that finds the dot for decimal numbers.
+     * @param line as string
+     * @return Number type as {@link MainPackage.InputType InputType}
+     */
     public static InputType checkNumberType(String line){
-        for(int i=0; i<line.length(); i++)
-            if(line.charAt(i)  == '.') // double or float
-                if(Float.parseFloat(line) > Float.MAX_VALUE
-                        || Float.parseFloat(line) < Float.MIN_VALUE) // double
+            if(prospectFloatNumber) { // double or float
+                float f = minusNumber? +Float.parseFloat(line) : -Float.parseFloat(line);
+                if(f == 0.0f && Double.parseDouble(line) != 0.0) // check double
                     return InputType.Double;
                 else // float
-                    return InputType.Float;
+                return InputType.Float;
+            }
         // integer or long or byte short
         if(Long.parseLong(line) > Integer.MAX_VALUE || Long.parseLong(line) < Integer.MIN_VALUE)
             return InputType.Long;
